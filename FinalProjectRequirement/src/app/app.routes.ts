31687278abbee68692ca19b978a63ff223ddf0f4
jonesmel <20228392@s.ubaguio.edu>
2025-04-28 11:14:59 +0800

    commit

--- FinalProjectRequirement/src/app/app.routes.ts
+++ FinalProjectRequirement/src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { GalleryComponent } from './components/gallery/gallery.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { FeedBackComponent } from './components/feedback/feedback.component';
 
-import { FeedbackComponent } from './components/feedback/feedback.component';
 
 export const routes: Routes = [
     {title: "ADS | Home", path:'' , component: HeaderComponent},
@@ -14,6 +14,6 @@ export const routes: Routes = [
     {title: "ADS | About", path:'about' , component: AboutComponent},
     {title: "ADS | Gallery", path:'gallery' , component: GalleryComponent},
     {title: "ADS | Not Found", path:'**' , component: NotfoundComponent},
-    {title: "ADS | Feedback", path:'feedback',component: FeedbackComponent}
+    {title: "ADS | Feedback", path:'feedback',component: FeedBackComponent}
 
 ];
