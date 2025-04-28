a1d263b09fc47974f940cc0fc5c6b0ec4d0a78fa
jonesmel <20228392@s.ubaguio.edu>
2025-04-28 11:14:21 +0800

    commit

--- FinalProjectRequirement/src/app/app.routes.ts
+++ FinalProjectRequirement/src/app/app.routes.ts
@@ -5,45 +5,15 @@ import { GalleryComponent } from './components/gallery/gallery.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { FeedbackComponent } from './components/feedback/feedback.component';
-=======
-<<<<<<< HEAD
-import { FeedbackComponent } from './components/feedback/feedback.component';
-=======
-import { FeedBackComponent } from './components/feedback/feedback.component';
 
->>>>>>> Jimenez_branch
-
->>>>>>> 6a2d8cc882936fb46b2faa8716dba6b5f09c74b8
-=======
-import { FeedBackComponent } from './components/feedback/feedback.component';
-
->>>>>>> f8219daaa23f6b99945765cd1b06fcc5b65b4677
 export const routes: Routes = [
     {title: "ADS | Home", path:'' , component: HeaderComponent},
     {title: "ADS | Home", path:'home' , component: HeaderComponent},
     {title: "ADS | About", path:'about' , component: AboutComponent},
     {title: "ADS | Gallery", path:'gallery' , component: GalleryComponent},
-<<<<<<< HEAD
-<<<<<<< HEAD
     {title: "ADS | Not Found", path:'**' , component: NotfoundComponent},
     {title: "ADS | Feedback", path:'feedback',component: FeedbackComponent}
-=======
-<<<<<<< HEAD
-    {title: "ADS | Feedback", path:'feedback', component:FeedbackComponent},
-    {title: "ADS | Not Found", path:'**' , component: NotfoundComponent}
-    
-=======
-    {title: "ADS | FeedBack", path:'feedback' , component: FeedBackComponent},
-    {title: "ADS | Not Found", path:'**' , component: NotfoundComponent}
-    
->>>>>>> 6a2d8cc882936fb46b2faa8716dba6b5f09c74b8
 
->>>>>>> Jimenez_branch
-=======
-    {title: "ADS | Feedback", path:'feedback', component:FeedBackComponent},
-    {title: "ADS | Not Found", path:'**' , component: NotfoundComponent}
->>>>>>> f8219daaa23f6b99945765cd1b06fcc5b65b4677
 ];
