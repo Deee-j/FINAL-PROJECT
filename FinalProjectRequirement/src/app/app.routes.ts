f8219daaa23f6b99945765cd1b06fcc5b65b4677
Deee-j <20228389@s.ubaguio.edu>
2025-04-28 11:09:20 +0800

    did some slight changes in the app component and routes to fix the errors

--- FinalProjectRequirement/src/app/app.routes.ts
+++ FinalProjectRequirement/src/app/app.routes.ts
@@ -5,27 +5,13 @@ import { GalleryComponent } from './components/gallery/gallery.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
-<<<<<<< HEAD
-import { FeedbackComponent } from './components/feedback/feedback.component';
-=======
 import { FeedBackComponent } from './components/feedback/feedback.component';
 
->>>>>>> Jimenez_branch
-
 export const routes: Routes = [
-
     {title: "ADS | Home", path:'' , component: HeaderComponent},
     {title: "ADS | Home", path:'home' , component: HeaderComponent},
     {title: "ADS | About", path:'about' , component: AboutComponent},
     {title: "ADS | Gallery", path:'gallery' , component: GalleryComponent},
-<<<<<<< HEAD
-    {title: "ADS | Feedback", path:'feedback', component:FeedbackComponent},
+    {title: "ADS | Feedback", path:'feedback', component:FeedBackComponent},
     {title: "ADS | Not Found", path:'**' , component: NotfoundComponent}
-    
-=======
-    {title: "ADS | FeedBack", path:'feedback' , component: FeedBackComponent},
-    {title: "ADS | Not Found", path:'**' , component: NotfoundComponent}
-    
-
->>>>>>> Jimenez_branch
 ];
