279bd430868dcca1f2ea8853b92635ce663d6a33
Deee-j <20228389@s.ubaguio.edu>
2025-04-29 23:48:58 +0800

    transformed the content of the gallery component into data binding and added a search component

--- FinalProjectRequirement/src/app/components/nav/nav.component.ts
+++ FinalProjectRequirement/src/app/components/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,RouterOutlet],
+  imports: [RouterLink,RouterLinkActive,RouterOutlet,SearchComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
