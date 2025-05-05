a4a44de7e5cea3e348cb785aad17ab2308df2242
jonesmel <20228392@s.ubaguio.edu>
2025-05-05 13:12:41 +0800

    commit

--- FinalProjectRequirement/src/app/components/admin/dashboard/dashboard.component.ts
+++ FinalProjectRequirement/src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GalleryComponent } from '../../gallery/gallery.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,GalleryComponent,RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
