ffa802626496a8de515c9196fe563fb6957f3bcb
Carpio-C <20228393@s.ubaguio.edu>
2025-05-06 17:03:44 +0800

    added image to copyright component

--- FinalProjectRequirement/src/app/components/footer/copyright/copyright.component.ts
+++ FinalProjectRequirement/src/app/components/footer/copyright/copyright.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class CopyrightComponent {
   title = 'Copyright Notice';
-
+  imageUrl = 'https://articles.unesco.org/sites/default/files/2023-09/GCM_Eng_Wheel_SDG4.pnghttps://www.voicesofyouth.org/sites/voy/files/styles/full_width_image/public/images/2021-01/21431207238_5eb3d94814_b_0.jpg?itok=1yZF-n8I'; 
   sectionContent: string[] = [
     `Copyright © <strong class="text-primary">EduKonek</strong><br>All rights reserved.`,
     `None of the materials provided on this web site may be used, reproduced or transmitted, in whole or in part, in any form or by any means, electronic or mechanical, including photocopying, recording or the use of any information storage and retrieval system, except as provided for in the <strong class="text-danger">Terms and Conditions of Use</strong> of <strong class="text-primary">EduKonek Web Sites</strong>, without permission in writing from the publisher.`,
